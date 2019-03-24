7f0c69a530c068265c7a449c2c0b6d8e5b37705b
党学义 <dangxy99@163.com>
2019-03-24 13:34:41 +0800

    add test

---
+++ app/src/main/java/com/dangxy/codestudy/java/Test.java
@@ -0,0 +1,43 @@
+package com.dangxy.codestudy.java;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String [] args){
+        //demo();
+        ThreadDemo threadDemo = new ThreadDemo();
+        threadDemo.start();
+        demo();
+    }
+
+    /**
+     * 测试方法
+     */
+    private static void demo() {
+        System.out.println("hello world");
+
+        List<String> list = new ArrayList<>();
+
+        list.add("test");
+        list.add("test1");
+        for (String str:list
+             ) {
+           System.out.println(str);
+        }
+
+    }
+
+
+    private static class ThreadDemo extends Thread{
+        @Override
+        public void run() {
+            super.run();
+
+            System.out.println(" Thread run");
+        }
+    }
+   }
